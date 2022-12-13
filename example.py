2f1358bea20bc6e13f43222866692a5a6a303181
marc-dantas <marciodantasms@hotmail.com>
2022-12-13 13:37:23 -0300

    mypy cache deletion

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from cuper.core import Parser, Type, Argument, Option
+from cuper.core import Parser, Type, Argument, Option, count
 
 p = Parser(["p", "the magic of python"])
 r = p.match([Option(["print", "p"]), Argument(Type.TEXT)])
